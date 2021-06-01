57d22c584e6a10e54b1d441f4d26a15a97b45898
Intern <intern@turntabl.io>
2021-06-01 03:06:04 -0700

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template,url_for,request,jsonify,redirect
+from flask import Flask , render_template,url_for,request,redirect
 from flask.scaffold import F
 from recipes import *
 import ast
